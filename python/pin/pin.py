cb76b949fa1acdd796d0e4090914f0dd564990f9
sandman <bmsandoval@gmail.com>
2021-03-14 20:47:23 -0600

    fixing some assertions

--- python/pin/pin.py
+++ python/pin/pin.py
@@ -9,6 +9,7 @@ inputs = {val : key
           for key, val
           in remap.items()}
 
+
 def get_pins(observed):
     return map(''.join,product(*[
         [
@@ -19,3 +20,10 @@ def get_pins(observed):
             and remap[(y1+y2,x1+x2)] != '-1'
         ] for y1,x1 in [ inputs[o] for o in observed ]
     ]))
+
+
+if __name__ == '__main__':
+    for input,output in [('8', ['5','7','8','9','0']),
+                 ('11',["11", "22", "44", "12", "21", "14", "41", "24", "42"]),
+                 ('369', ["339","366","399","658","636","258","268","669","668","266","369","398","256","296","259","368","638","396","238","356","659","639","666","359","336","299","338","696","269","358","656","698","699","298","236","239"])]:
+        assert(sorted(get_pins(input)) == sorted(output))

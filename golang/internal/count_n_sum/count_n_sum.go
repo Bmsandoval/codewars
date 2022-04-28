b53f444b508efdba0a017d591108907ba451b499
sandman <bmsandoval@gmail.com>
2022-04-28 12:45:35 -0700

    docs: added detailed comments

--- golang/internal/count_n_sum/count_n_sum.go
+++ golang/internal/count_n_sum/count_n_sum.go
@@ -17,14 +17,20 @@ func CountNSum(numbers []int) []int {
 		return []int{}
 	}
 
+	// create a channel for the two handlers to place their responses
 	resultCh := make(chan int, 2)
+	// create a channel to pipe positive inputs to appropriate handler
 	posValCh := make(chan int)
+	// create a channel to pipe negative inputs to appropriate handler
 	negValCh := make(chan int)
 
+	// wait group will be used to track when the handlers are done processing inputs
 	wg.Add(2)
+	// launch our two handler subroutines in the background
 	go negativesHandler(resultCh, negValCh)
 	go positivesHandler(resultCh, posValCh)
 
+	// loop over the numbers and send positives and negatives to their rightful place
 	for _, val := range numbers {
 		if val > 0 {
 			posValCh <- val
@@ -32,9 +38,13 @@ func CountNSum(numbers []int) []int {
 			negValCh <- val
 		}
 	}
+	// tell the positives handler that we are done sending inputs
 	close(posValCh)
+	// tell the negatives handler the same
 	close(negValCh)
+	// wait for the two handlers to finish processing their inputs
 	wg.Wait()
+	// close the channel of results (so we can freely 'range' over it)
 	close(resultCh)
 	resp := []int{0, 0}
 	for v := range resultCh {
@@ -47,20 +57,28 @@ func CountNSum(numbers []int) []int {
 	return resp
 }
 
+// positivesHandler accepts any number or positive values and will return a count of them
 func positivesHandler(resultCh chan<- int, inputsCh <-chan int) {
 	count := 0
+	// handle inputs until the channel is empty&closed
 	for range inputsCh {
 		count++
 	}
+	// post results when you have them
 	resultCh <- count
+	// notify main thread that we are done
 	wg.Done()
 }
 
+// negativesHandler accepts any number of negative values and will return their sum
 func negativesHandler(resultCh chan<- int, inputsCh <-chan int) {
 	sum := 0
+	// handle inputs until the channel is empty&closed
 	for val := range inputsCh {
 		sum += val
 	}
+	// post results when you have them
 	resultCh <- sum
+	// notify main thread that we are done
 	wg.Done()
 }

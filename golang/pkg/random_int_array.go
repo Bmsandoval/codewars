5604af0e93c1516412611a539b8b2851be921e95
sandman <bmsandoval@gmail.com>
2022-04-28 13:05:21 -0700

    feat: added a randomizer to use in my benchmarks

--- golang/pkg/random_int_array.go
+++ golang/pkg/random_int_array.go
@@ -1,32 +1,9 @@
 package pkg
 
-import (
-	"math/rand"
-	"time"
-	"unsafe"
-)
-
-const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const (
-	letterIdxBits = 6                    // 6 bits to represent a letter index
-	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
-	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
-)
-var src = rand.NewSource(time.Now().UnixNano())
-func RandomString(n int) string {
-	b := make([]byte, n)
-	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
-	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
-		if remain == 0 {
-			cache, remain = src.Int63(), letterIdxMax
-		}
-		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
-			b[i] = letterBytes[idx]
-			i--
-		}
-		cache >>= letterIdxBits
-		remain--
+func RandomIntArray(n int) []int {
+	var arr = make([]int, n)
+	for i := 0; i < n; i++ {
+		arr = append(arr, int(src.Int63()))
 	}
-
-	return *(*string)(unsafe.Pointer(&b))
+	return arr
 }

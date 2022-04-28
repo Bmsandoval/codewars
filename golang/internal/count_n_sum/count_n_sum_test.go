2aa5100af4bdebbffe367a7cb9084234dd950756
sandman <bmsandoval@gmail.com>
2022-04-28 13:04:56 -0700

    test: added unit tests

--- golang/internal/count_n_sum/count_n_sum_test.go
+++ golang/internal/count_n_sum/count_n_sum_test.go
@@ -5,58 +5,48 @@ import (
 	"testing"
 )
 
-func TestIpValidation(t *testing.T) {
+func TestCountNSum(t *testing.T) {
 	unitTestData := []struct {
 		Description string
-		Input       string
-		Expect      bool
+		Input       []int
+		Expect      []int
 	}{
 		{
-			Description: "happy path (1)",
-			Input:       "1.2.3.4",
-			Expect:      true,
+			Description: "happy path",
+			Input:       []int{10, -10},
+			Expect:      []int{1, -10},
 		},
 		{
-			Description: "happy path (2)",
-			Input:       "123.45.67.89",
-			Expect:      true,
+			Description: "order shouldn't matter",
+			Input:       []int{-10, 10},
+			Expect:      []int{1, -10},
 		},
 		{
-			Description: "can detect not enough octets",
-			Input:       "1.2.3",
-			Expect:      false,
+			Description: "can handle empty array",
+			Input:       []int{},
+			Expect:      []int{},
 		},
 		{
-			Description: "can detect too many octets",
-			Input:       "1.2.3.4.5",
-			Expect:      false,
+			Description: "can handle missing negatives",
+			Input:       []int{10},
+			Expect:      []int{1, 0},
 		},
 		{
-			Description: "can detect large octet values",
-			Input:       "123.456.78.90",
-			Expect:      false,
+			Description: "can handle missing positives",
+			Input:       []int{-10},
+			Expect:      []int{0, -10},
 		},
 		{
-			Description: "can detect leading zeros in octet values",
-			Input:       "123.045.067.089",
-			Expect:      false,
+			Description: "longer example",
+			Input:       []int{20, -2, 47, -11, 33, -17, 100, -1},
+			Expect:      []int{4, -31},
 		},
 	}
 
 	for _, testData := range unitTestData {
 		t.Run(testData.Description, func(t *testing.T) {
-			got := IpValidation(testData.Input)
+			got := CountNSum(testData.Input)
 			assert.Equal(t, testData.Expect, got)
 		})
 	}
-}
-
-var result bool
-
-func BenchmarkValidBraces(b *testing.B) {
-	var r bool
-	for n := 0; n < b.N; n++ {
-		r = IpValidation("1.2.3.4")
-	}
-	result = r
 }

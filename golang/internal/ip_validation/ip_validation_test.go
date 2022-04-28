3657439917d9b97d218888e15038bf9b36cced57
sandman <bmsandoval@gmail.com>
2022-04-28 11:35:08 -0700

    test: adding unit tests

--- golang/internal/ip_validation/ip_validation_test.go
+++ golang/internal/ip_validation/ip_validation_test.go
@@ -5,52 +5,58 @@ import (
 	"testing"
 )
 
-func TestValidBraces(t *testing.T) {
+func TestIpValidation(t *testing.T) {
 	unitTestData := []struct {
 		Description string
-		Input string
-		Expect bool
+		Input       string
+		Expect      bool
 	}{
 		{
-			Description: "immediate match",
-			Input: "(){}[]",
-			Expect: true,
+			Description: "happy path (1)",
+			Input:       "1.2.3.4",
+			Expect:      true,
 		},
 		{
-			Description: "wrapping match",
-			Input: "{([])}",
-			Expect: true,
+			Description: "happy path (2)",
+			Input:       "123.45.67.89",
+			Expect:      true,
 		},
 		{
-			Description: "unmatched",
-			Input: "(}",
-			Expect: false,
+			Description: "can detect not enough octets",
+			Input:       "1.2.3",
+			Expect:      false,
 		},
 		{
-			Description: "mismatched",
-			Input: "[(])",
-			Expect: false,
+			Description: "can detect too many octets",
+			Input:       "1.2.3.4.5",
+			Expect:      false,
 		},
 		{
-			Description: "extras",
-			Input: "[({})](]",
-			Expect: false,
+			Description: "can detect large octet values",
+			Input:       "123.456.78.90",
+			Expect:      false,
+		},
+		{
+			Description: "can detect leading zeros in octet values",
+			Input:       "123.045.067.089",
+			Expect:      false,
 		},
 	}
 
 	for _, testData := range unitTestData {
 		t.Run(testData.Description, func(t *testing.T) {
-			got := ValidBraces(testData.Input)
+			got := IpValidation(testData.Input)
 			assert.Equal(t, testData.Expect, got)
 		})
 	}
 }
 
 var result bool
+
 func BenchmarkValidBraces(b *testing.B) {
 	var r bool
 	for n := 0; n < b.N; n++ {
-		r = ValidBraces("[{([](){})}{}()[]]")
+		r = IpValidation("1.2.3.4")
 	}
 	result = r
 }
